c491ead7d024104c6be405c3dc5f36889de2dd91
Hd Böhlau <hans-dieter.boehlau@incowia.com>
2015-12-14 08:46:21 +0100

    PLAT-129: initial commit

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Cubixx Base - Authentication Service' });
+});
+
+module.exports = router;
